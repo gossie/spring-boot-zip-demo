9c914ccec97bdb456b5e3a8f8bad7caf9590233c
André Schreck <moin@andreschreck.de>
2022-04-10 18:42:50 +0200

    write temp files before adding them to zip

--- src/main/java/com/github/gossie/zipdemo/FileService.java
+++ src/main/java/com/github/gossie/zipdemo/FileService.java
@@ -4,13 +4,31 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
 
 @Service
 public class FileService {
 
     public void addFile(OutputStream out) throws IOException {
-        out.write("Hallo, das ist der Inhaltder Datei.".getBytes(StandardCharsets.UTF_8));
+        Path tempFilePath = null;
+        try {
+            tempFilePath = writeToTempFile(UUID.randomUUID().toString());
+            addTempFileToOutputStream(tempFilePath, out);
+        } finally {
+            if (tempFilePath != null) {
+                Files.deleteIfExists(tempFilePath);
+            }
+        }
+    }
+
+    private Path writeToTempFile(String filename) throws IOException {
+        return Files.writeString(Files.createTempFile(filename, ".txt"), "Hallo, das ist der Inhalt der Datei " + filename + ".txt.");
+    }
+
+    private void addTempFileToOutputStream(Path tempFilePath, OutputStream out) throws IOException {
+        out.write(Files.readAllBytes(tempFilePath));
     }
 
 }
